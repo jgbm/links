f4f0b0a43b9c2c54e7d84e26656bab13a3313305
J. Garrett Morris <Garrett.Morris@ed.ac.uk>
2015-05-15 12:36:57 +0100

    Treat cookies like other CGI parameters.

--- webif.ml
+++ webif.ml
@@ -389,7 +389,20 @@ let serve_request ((valenv,nenv,tyenv) as envs) prelude filename =
 
   let cgi_args = get_cgi_args() in
   Lib.cgi_parameters := cgi_args;
-
+  Lib.cookies :=
+    begin
+      match getenv "HTTP_COOKIE" with
+      | Some header ->
+         let cookies = Str.split (Str.regexp "[ \t]*;[ \t]*") header in
+         concat_map
+           (fun str ->
+            match Str.split (Str.regexp "[ \t]*=[ \t]*") str with
+            | [nm; vl] -> [nm, vl]
+            | _ -> Debug.print ("Warning: ill-formed cookie: "^str); [])
+           cookies
+      | None ->
+         []
+    end;
 
   (* Compute cacheable stuff in one call *)
 
